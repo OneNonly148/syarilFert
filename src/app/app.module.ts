eeeef1238b1eabc106feb3d26729e5d19855a0ce
Wan Akashah <wanakashah14833@gmail.com>
2020-12-09 16:52:07 +0800

    Initialized project with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDpqOfb2eGvuNXyIcs46-RWPgBLRboBL6k",
+    authDomain: "mit-fertigation.firebaseapp.com",
+    databaseURL: "https://mit-fertigation.firebaseio.com",
+    projectId: "mit-fertigation",
+    storageBucket: "mit-fertigation.appspot.com",
+    messagingSenderId: "324408976147",
+    appId: "1:324408976147:web:e1e4845142d6018cf470e6",
+    measurementId: "G-J2VG6T0XLL"
+  }
 };
 
 /*
