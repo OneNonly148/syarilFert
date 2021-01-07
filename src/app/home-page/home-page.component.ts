ebd6e0552bce5b7f55def1401302cc0a8e7bcdb6
Wan Akashah <wanakashah14833@gmail.com>
2021-01-08 00:25:35 +0800

    Prepare dashboard for 8btn n 4sensor readout

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -22,11 +22,15 @@ export class HomePageComponent implements OnInit {
   btnStat2=false;
   btnStat3=false;
   btnStat4=false;
+  btnStat5=false;
+  btnStat6=false;
+  btnStat7=false;
+  btnStat8=false;
   selection = {
     value : '',
   };
   constructor(public auth: AngularFireAuth,db: AngularFireDatabase,private router : Router) {
-    
+
     this.datas = db.list('/fertigation/location').valueChanges();
     this.datas.forEach(x => {
       this.data = x;
@@ -37,58 +41,103 @@ export class HomePageComponent implements OnInit {
       this.btn = x;
       console.log(x);
       this.btnStat1=this.btn[0];
-    console.log(this.btnStat1);
-    })      
-    
+      this.btnStat2=this.btn[1];
+      this.btnStat3=this.btn[2];
+      this.btnStat4=this.btn[3];
+      this.btnStat5=this.btn[4];
+      this.btnStat6=this.btn[5];
+      this.btnStat7=this.btn[6];
+      this.btnStat8=this.btn[7];
+    })
+
   }
   login() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
   buttonP(btn:number) {
     console.log(btn);
-    
     switch(btn){
       case 1:
         if(this.btnStat1 ){
-          console.log('off');
-          this.btnStat1 = !this.btnStat1; 
-          
+          console.log('1 off');
+          this.btnStat1 = !this.btnStat1;
+
         }else{
-          console.log('on');
-          this.btnStat1 = !this.btnStat1; 
+          console.log('1 on');
+          this.btnStat1 = !this.btnStat1;
         }
         firebase.database().ref('/fertigation/btn').update({ d1: this.btnStat1 });
         break;
       case 2:
         if(this.btnStat2){
-          console.log('off');
-          this.btnStat2 = !this.btnStat2; 
+          console.log('2 off');
+          this.btnStat2 = !this.btnStat2;
         }else{
-          console.log('on');
-          this.btnStat2 = !this.btnStat2; 
+          console.log('2 on');
+          this.btnStat2 = !this.btnStat2;
         }
         firebase.database().ref('/fertigation/btn').update({ d2: this.btnStat2 });
         break;
       case 3:
         if(this.btnStat3){
-          console.log('off');
-          this.btnStat3 = !this.btnStat3; 
+          console.log('3 off');
+          this.btnStat3 = !this.btnStat3;
         }else{
-          console.log('on');
-          this.btnStat3 = !this.btnStat3; 
+          console.log('3 on');
+          this.btnStat3 = !this.btnStat3;
         }
         firebase.database().ref('/fertigation/btn').update({ d3: this.btnStat3 });
         break;
-        case 4:
-          if(this.btnStat4){
-            console.log('off');
-            this.btnStat4 = !this.btnStat4; 
-          }else{
-            console.log('on');
-            this.btnStat4 = !this.btnStat4; 
-          }
-          firebase.database().ref('/fertigation/btn').update({ d4: this.btnStat4 });
-          break;
+      case 4:
+        if(this.btnStat4){
+          console.log('4 off');
+          this.btnStat4 = !this.btnStat4;
+        }else{
+          console.log('4 on');
+          this.btnStat4 = !this.btnStat4;
+        }
+        firebase.database().ref('/fertigation/btn').update({ d4: this.btnStat4 });
+        break;
+      case 5:
+        if(this.btnStat5){
+          console.log('5 off');
+          this.btnStat5 = !this.btnStat5;
+        }else{
+          console.log('5 on');
+          this.btnStat5 = !this.btnStat5;
+        }
+        firebase.database().ref('/fertigation/btn').update({ d5: this.btnStat5 });
+        break;
+      case 6:
+        if(this.btnStat6){
+          console.log('6 off');
+          this.btnStat6 = !this.btnStat6;
+        }else{
+          console.log('6 on');
+          this.btnStat6 = !this.btnStat6;
+        }
+        firebase.database().ref('/fertigation/btn').update({ d6: this.btnStat6 });
+        break;
+      case 7:
+        if(this.btnStat7){
+          console.log('7 off');
+          this.btnStat7 = !this.btnStat7;
+        }else{
+          console.log('7 on');
+          this.btnStat7 = !this.btnStat7;
+        }
+        firebase.database().ref('/fertigation/btn').update({ d7: this.btnStat7 });
+        break;
+      case 8:
+        if(this.btnStat8){
+          console.log('8 off');
+          this.btnStat8 = !this.btnStat8;
+        }else{
+          console.log('8 on');
+          this.btnStat8 = !this.btnStat8;
+        }
+        firebase.database().ref('/fertigation/btn').update({ d8: this.btnStat8 });
+        break;
     }
   }
   ngOnInit(): void {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDpqOfb2eGvuNXyIcs46-RWPgBLRboBL6k",
+    authDomain: "mit-fertigation.firebaseapp.com",
+    databaseURL: "https://mit-fertigation.firebaseio.com",
+    projectId: "mit-fertigation",
+    storageBucket: "mit-fertigation.appspot.com",
+    messagingSenderId: "324408976147",
+    appId: "1:324408976147:web:e1e4845142d6018cf470e6",
+    measurementId: "G-J2VG6T0XLL"
+  }
 };
