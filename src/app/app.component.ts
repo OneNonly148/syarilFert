b9def3c722cc838210ad596986c358dc3c9e1e15
OneNonly148 <wanakashah14833@gmail.com>
2021-02-27 15:00:41 +0800

    Saving work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 import {Router} from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,41 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public auth: AngularFireAuth,private router : Router) {
+  closeResult = '';
+  dispNumber:number = 0;
+  dispStat = false;
+  constructor(public auth: AngularFireAuth,private router : Router,private modalService: NgbModal,) {
   }
+
+  disbDisp(){
+    this.dispStat = false;
+  }
+
+  autoDisp(content:any){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.dispStat = true;
+      console.log(this.closeResult + ' ' + this.dispNumber);
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
   logout() {
     this.auth.signOut();
     console.log('logout');
   }
+
   setting(){
     console.log('helo');
     this.router.navigate(['setting']);
