ed5d81c668d0c853e99e60685d8ea9f3e4f0336f
OneNonly148 <wanakashah14833@gmail.com>
2021-05-24 12:44:21 +0800

    Attempt to fix current change but failed

--- src/app/datalog/datalog.component.ts
+++ src/app/datalog/datalog.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { from } from 'rxjs';
-import { saveAs } from 'file-saver';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 
 export interface tData {
   time: number;
@@ -30,45 +27,10 @@ export class DatalogComponent implements OnInit {
 
   getTable(){
     console.log('Getting table');
-    this.downReady = false;
-    if(this.range.value.start && this.range.value.end){
-      // this.graphP.getDateTable(this.par.loc,this.range.value.start,this.range.value.end);
-      // const sub2 = this.graphP.tableDate$.subscribe(value => {
-      //   console.log(value);
-      //   if(value.length>0){
-      //     console.log(value);
-      //     this.tableData=value;
-      //     this.downReady = true;
-      //   }
-      //   setTimeout(() => {
-      //     sub2.unsubscribe();
-      //     this.downReady = true;
-      //   }, 5000);
-      // });
-    }else{
-      console.log('Invalid date');
-    }
   }
 
   getDownload(data: any){
     console.log('Downloading');
-    // const replacer = (key, value) => (value === null ? '' : value); // specify how you want to handle null values here
-    // const header = Object.keys(data[0]);
-    // const csv = data.map((row) =>
-    //   header
-    //     .map((fieldName) => JSON.stringify(row[fieldName], replacer))
-    //     .join(',')
-    // );
-    // csv.unshift(header.join(','));
-    // const csvArray = csv.join('\r\n');
-    // const a = document.createElement('a');
-    // const blob = new Blob([csvArray], { type: 'text/csv' });
-    // const url = window.URL.createObjectURL(blob);
-    // a.href = url;
-    // a.download = 'myFile.csv';
-    // a.click();
-    // window.URL.revokeObjectURL(url);
-    // a.remove();
   }
 
   ngOnInit(): void {
