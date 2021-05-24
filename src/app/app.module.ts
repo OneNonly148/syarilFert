fc4c3e48989ca1d6b5bfa970a777e4d9b627bd20
OneNonly148 <wanakashah14833@gmail.com>
2021-05-24 11:29:24 +0800

    Save data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,22 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { DatalogComponent } from './datalog/datalog.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,6 +48,22 @@ import { DatalogComponent } from './datalog/datalog.component';
     ]),
     NgbModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/datalog/datalog.component.ts
+++ src/app/datalog/datalog.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+export interface tData {
+  time: number;
+  temp: number;
+  humd: number;
+  irrd: number;
+  power: number;
+}
 
 @Component({
   selector: 'app-datalog',
@@ -6,8 +15,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datalog.component.css']
 })
 export class DatalogComponent implements OnInit {
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+  downReady = false;
+  tableData: Array<tData> = [];
+  displayedColumns: string[] = ['time', 'temp', 'humd', 'irrd', 'power'];
 
   constructor() { }
+
+  getTable(){
+    console.log('Getting table');
+    this.downReady = false;
+    if(this.range.value.start && this.range.value.end){
+      this.graphP.getDateTable(this.par.loc,this.range.value.start,this.range.value.end);
+      const sub2 = this.graphP.tableDate$.subscribe(value => {
+        console.log(value);
+        if(value.length>0){
+          console.log(value);
+          this.tableData=value;
+          this.downReady = true;
+        }
+        setTimeout(() => {
+          sub2.unsubscribe();
+          this.downReady = true;
+        }, 5000);
+      });
+    }else{
+      console.log('Invalid date');
+    }
+  }
+
+  getDownload(data: any){
+    console.log('Downloading');
+    const replacer = (key, value) => (value === null ? '' : value); // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    const csv = data.map((row) =>
+      header
+        .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+        .join(',')
+    );
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+    const a = document.createElement('a');
+    const blob = new Blob([csvArray], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = 'myFile.csv';
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
 
   ngOnInit(): void {
   }
